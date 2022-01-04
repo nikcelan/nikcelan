675b1add726287ded2eb605ef8452c86d9575e8d
nikcelan <97116691+nikcelan@users.noreply.github.com>
2022-01-04 18:11:35 +0100

    Add files via upload

---
+++ sum_oneline.py
@@ -0,0 +1,5 @@
+from numpy import subtract
+
+print(sum ([int(i) for i in input("vnesite stevilko:").split("+")]))
+
+print(subtract ([int(i) for i in input("vnesite stevilko:")], [int(i) for i in input("vnesite stevilko:")]))
